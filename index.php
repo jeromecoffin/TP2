7aabbe0a5021e72f8455477ec9b0da3cb4e72d23
Unknown <jerome.coffin.20160034@efrei.net>
2019-05-13 15:53:15 +0200

    first commit
    index and style files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Ma première page</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Ma première page ...</h1>
+        <p>Mon premier paragraphe</p>
+        <?php 
+            echo "Du texte en php";
+        ?>
+    </body>
+</html>
+
+
